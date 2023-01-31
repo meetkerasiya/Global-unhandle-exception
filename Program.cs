fc0731ce8867cf23a69c9c46fb4e3c0aa1b546af
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-01-31 09:54:26 +0530

    Global handler using AppDomain

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class MyClass
+{
+    public static void Main(string[] arguments)
+    {
+        AppDomain currentDomain= AppDomain.CurrentDomain;
+        currentDomain.UnhandledException += new UnhandledExceptionEventHandler(
+            (object sender,UnhandledExceptionEventArgs args) =>
+            {
+                Exception e = (Exception)args.ExceptionObject;
+                Console.WriteLine("Global Handler caught : "+e.Message);
+                Console.WriteLine("RunTime terminating: {0}", args.IsTerminating);
+            });
+
+        try
+        {
+            throw new Exception("1");
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Catch clause caught : {0} \n",e.Message);
+
+        }
+        throw new Exception("2");
+    }
+}
